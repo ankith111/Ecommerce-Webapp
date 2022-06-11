56222a1535ef9b9e346a16ca9d313b04ec7489b3
ankith64 <ankithakshith@gmail.com>
2022-06-11 07:27:17 +0530

    Trade updated

--- frontend/src/Components/Trade/Trade.js
+++ frontend/src/Components/Trade/Trade.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "react";
+import JSONDATA from "../data.json";
 import i from "./i.svg";
 import Button from "../../Utilities/Components/Button/Button";
 import "../Logistics/LogisticsMainPage.scss";
 import Sidebar from "../../Utilities/Components/Sidebar/Sidebar";
 import "./Trade.scss";
 function Trade() {
+
+  const [sterm,setsterm] = useState("");
   return (
+
     <div className="mainPage">
       <Sidebar />
       <div className="m1">
@@ -22,6 +26,7 @@ function Trade() {
             </div>
           </div>
         </div>
+        
 
         <div className="f3">
           <form className="form">
@@ -29,10 +34,12 @@ function Trade() {
               <label for="country" className="f-sx fw-m lab">
                 Origin country:
               </label>
-              <select className="ph" name="country" id="cars">
-                <option
+              
+              <select className="ph " name="country" id="cars">
+              <option   value="" disabled selected>Country name</option>
+                <option 
                   placeholder="Country"
-                  className="op ff-poppins f-sx fw-m"
+                  className="op ff-poppins f-sx fw-m ph."
                   value="volvo"
                 >
                   USA
@@ -52,7 +59,9 @@ function Trade() {
               <label for="country" className="f-sx fw-m lab">
                 Destination country:
               </label>
+              
               <select className="ph" name="country" id="cars">
+              <option className="uno"  value="" disabled selected>Country name</option>
                 <option className="op ff-poppins f-sx fw-m" value="volvo">
                   USA
                 </option>
@@ -63,24 +72,45 @@ function Trade() {
               </select>
               <div className="usd"></div>
             </div>
-            <div className="input">
+
+              <div class="ins">
               <label for="country" className="f-sx fw-m lab">
                 Describe your Product:
               </label>
-              <select className="ph" name="country" id="cars">
-                <option className="op ff-poppins f-sx fw-m" value="volvo">
-                  Volvo
-                </option>
-                <option className="op ff-poppins f-sx fw-m" value="saab">
-                  Saab
-                </option>
-                <option className="op ff-poppins f-sx fw-m" value="opel">
-                  Opel
-                </option>
-                <option className="op ff-poppins f-sx fw-m" value="audi">
-                  Audi
-                </option>
-              </select>
+              <div className="inputsp">
+              <input
+              type="text"
+              placeholder="search"
+              className="insp" 
+              onChange={(event)=>{
+                setsterm(event.target.value);
+              }}
+              >
+              </input>
+              {
+                <div className="sheet">
+                {JSONDATA.filter((val)=>{
+                  if(sterm==""){
+                  
+                  }
+                  else if(val.toLowerCase().includes(sterm.toLowerCase())){
+                    return val;
+                  }
+                }).map((val,key)=>{
+                  return(
+                    <div className="user">
+                      <p>{val}</p>
+                    </div>
+                  )
+                })}
+                </div>
+              }
+             </div>
+
+
+
+
+
               <div className="usd"></div>
             </div>
             <div className="input">
@@ -106,30 +136,22 @@ function Trade() {
                 Construction:
               </label>
               <select className="ph" name="country" id="cars">
+              <option className="uno"  value="" disabled selected>Please select an option</option>
                 <option
-                  placeholder="Country"
+               
                   className="op ff-poppins f-sx fw-m"
                   value="volvo"
-                >
-                  Volvo
+                >Knitted or croching
+                  
                 </option>
                 <option
-                  placeholder="Country"
+                 
                   className="op ff-poppins f-sx fw-m"
                   value="saab"
                 >
-                  Saab
+                  other
                 </option>
-                <option
-                  placeholder="Country"
-                  className="op ff-poppins f-sx fw-m"
-                  value="opel"
-                >
-                  Opel
-                </option>
-                <option className="op ff-poppins f-sx fw-m" value="audi">
-                  Audi
-                </option>
+               
               </select>
               <div className="usd"></div>
             </div>
@@ -137,19 +159,19 @@ function Trade() {
               <label for="country" className="f-sx fw-m lab">
                 Gender:
               </label>
+            
               <select className="ph" name="country" id="cars">
+              <option className="uno"  value="" disabled selected>Please select an option</option>
                 <option className="op ff-poppins f-sx fw-m" value="volvo">
-                  Volvo
+                  Men's or Boys
                 </option>
                 <option className="op ff-poppins f-sx fw-m" value="saab">
-                  Saab
+                  Women's or girls
                 </option>
                 <option className="op ff-poppins f-sx fw-m" value="opel">
-                  Opel
+                  Babies
                 </option>
-                <option className="op ff-poppins f-sx fw-m" value="audi">
-                  Audi
-                </option>
+                
               </select>
               <div className="usd"></div>
             </div>
@@ -194,30 +216,7 @@ function Trade() {
               </div>
             </div>
 
-            {/* <table className="padding-table-columns">
-  <tr >
-   <div><th align="left">Schedule B Number</th></div> 
-    <th align="left">Description</th>
-    <th align="left">UOM</th>
-  </tr>
-  <tr  >
-    <td>61.04</td>
-    <td>Women's or girls' suits, ensembles, suit-type jackets, blazers. dresses, skirts, divided skirts, trousers, bib and brace overalls, breeches and shorts (other than swimwear) knitted or crocheted:</td>
-    <td></td>
-  </tr>
-  <tr >
-    <td></td>
-    <td>-Dresses</td>
-    <td></td>
-  </tr>
-  <tr >
-    <td>61.04</td>
-    <td>--of cotton</td>
-    <td>Doz.,kg</td>
-  </tr>
-  
-</table>
-           </div> */}
+           
      <div className="sb">
             <div className=" u1 fw-b">
            
